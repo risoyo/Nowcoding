95d1a417ce9f683e9ca29f1469c1fc8cd1cedbcc
yinglongnvba <yinglongnvba@163.com>
2020-02-05 10:40:54 +0800

    修改版式

--- community-frontend/src/views/Home.vue
+++ community-frontend/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-container>
+  <el-header>Header</el-header>
+  <el-main>
+    <home-main></home-main>
+  </el-main>
+  <el-footer>Footer</el-footer>
+</el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeMain from "@/components/Home/HomeMain.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HomeMain
   }
 };
 </script>
