dc09b4706dae66e0eac61aa5162b10139ca99337
risoyo <yinglongnvba@gmail.com>
2020-02-20 22:31:26 +0800

    生成带Claim与无Claim的token

--- src/test/java/com/nowcoder/community/JWTTests.java
+++ src/test/java/com/nowcoder/community/JWTTests.java
@@ -1,7 +1,10 @@
 package com.nowcoder.community;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.nowcoder.community.util.DateUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringRunner.class)
@@ -33,7 +37,74 @@ public class JWTTests {
     }
 
     @Test
-    public void createToken() {
+    public void createTokenWithoutClaim() {
+
+        String secret = "secret";// token 密钥
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+
+        // 头部信息
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+
+        Date nowDate = new Date();
+        Date expireDate =  dateUtils.getAfterTime(nowDate,30);// 2小过期
+
+        String token = JWT.create()
+                .withHeader(map)// 设置头部信息 Header
+                .withIssuer("SERVICE")//设置 载荷 签名是有谁生成 例如 服务器
+                .withSubject("this is test token")//设置 载荷 签名的主题
+                // .withNotBefore(new Date())//设置 载荷 定义在什么时间之前，该jwt都是不可用的.
+                .withAudience("APP")//设置 载荷 签名的观众 也可以理解谁接受签名的
+                .withIssuedAt(nowDate) //设置 载荷 生成签名的时间
+                .withExpiresAt(expireDate)//设置 载荷 签名过期的时间
+                .sign(algorithm);//签名 Signature
+        Assert.assertTrue(token.length() > 0);
+        System.out.println(token);
+    }
+
+    @Test
+    public void createTokenWithClaim(){
+        Date nowDate = new Date();
+        Date expireDate = dateUtils.getAfterTime(nowDate,30);// 2小过期
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        String token = JWT.create().withHeader(map)
+                /* 设置 载荷 Payload */
+                .withClaim("loginName", "zhuoqianmingyue").withClaim("userName", "张三").withClaim("deptName", "技术部")
+                .withIssuer("SERVICE")// 签名是有谁生成 例如 服务器
+                .withSubject("this is test token")// 签名的主题
+                // .withNotBefore(new Date())//定义在什么时间之前，该jwt都是不可用的
+                .withAudience("APP")// 签名的观众 也可以理解谁接受签名的
+                .withIssuedAt(nowDate) // 生成签名的时间
+                .withExpiresAt(expireDate)// 签名过期的时间
+                /* 签名 Signature */
+                .sign(algorithm);
+
+        Assert.assertTrue(token.length() > 0);
+        System.out.println(token);
+    }
+
+
+    @Test
+    public void verifyTokenWithoutClaim(){
+        String token = generateTokenWithoutClaim();
+
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        JWTVerifier verifier = JWT.require(algorithm).withIssuer("SERVICE").build(); // Reusable verifier instance
+        DecodedJWT jwt = verifier.verify(token);
+
+        String subject = jwt.getSubject();
+        List<String> audience = jwt.getAudience();
+        System.out.println(subject);
+        System.out.println(audience.get(0));
+
+    }
+    public String generateTokenWithoutClaim() {
 
         String secret = "secret";// token 密钥
         Algorithm algorithm = Algorithm.HMAC256("secret");
@@ -57,5 +128,54 @@ public class JWTTests {
                 .sign(algorithm);//签名 Signature
         Assert.assertTrue(token.length() > 0);
         System.out.println(token);
+        return token;
+    }
+
+    @Test
+    public void verifyTokenWithClaim(){
+        String token = generateTokenWithClaim();
+
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        JWTVerifier verifier = JWT.require(algorithm).withIssuer("SERVICE").build(); // Reusable verifier instance
+        DecodedJWT jwt = verifier.verify(token);
+
+        String subject = jwt.getSubject();
+        List<String> audience = jwt.getAudience();
+        Map<String, Claim> claims = jwt.getClaims();
+        for (Map.Entry<String, Claim> entry : claims.entrySet()) {
+            String key = entry.getKey();
+            Claim claim = entry.getValue();
+            System.out.println("key:" + key + " value:" + claim.asString());
+        }
+        Claim claim = claims.get("loginName");
+        System.out.println(subject);
+        System.out.println(audience.get(0));
+
+    }
+    public String generateTokenWithClaim() {
+
+        Date nowDate = new Date();
+        Date expireDate = dateUtils.getAfterTime(nowDate,30);// 2小过期
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        String token = JWT.create().withHeader(map)
+                /* 设置 载荷 Payload */
+                .withClaim("loginName", "zhuoqianmingyue").withClaim("userName", "张三").withClaim("deptName", "技术部")
+                .withIssuer("SERVICE")// 签名是有谁生成 例如 服务器
+                .withSubject("this is test token")// 签名的主题
+                // .withNotBefore(new Date())//定义在什么时间之前，该jwt都是不可用的
+                .withAudience("APP")// 签名的观众 也可以理解谁接受签名的
+                .withIssuedAt(nowDate) // 生成签名的时间
+                .withExpiresAt(expireDate)// 签名过期的时间
+                /* 签名 Signature */
+                .sign(algorithm);
+
+        Assert.assertTrue(token.length() > 0);
+        System.out.println(token);
+        return token;
     }
 }
