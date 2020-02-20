2e7e0f95a1d59dbf5f0445cf899f8b6953c3f818
risoyo <yinglongnvba@gmail.com>
2020-02-20 22:12:46 +0800

    整合JWT

---
+++ src/main/java/com/nowcoder/community/util/DateUtils.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.util;
+
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class DateUtils {
+    public void getCurrentTime(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        System.out.println("当前时间：" + sdf.format(now));
+    }
+
+    public Date getAfterTime(Date date,int minutes){
+        long time = minutes*60*1000;//30分钟
+        return new Date(date .getTime() + time);//30分钟后的时间
+    }
+}
---
+++ src/test/java/com/nowcoder/community/JWTTests.java
@@ -0,0 +1,61 @@
+package com.nowcoder.community;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.nowcoder.community.util.DateUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)  // 指名运行的测试代码的配置类
+public class JWTTests {
+    @Autowired
+    private DateUtils dateUtils;
+
+    @Test
+    public void dateTest(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        System.out.println("当前时间：" + sdf.format(now));
+        Date afterDate = dateUtils.getAfterTime(now,30);
+        System.out.println(sdf.format(afterDate ));
+    }
+
+    @Test
+    public void createToken() {
+
+        String secret = "secret";// token 密钥
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+
+        // 头部信息
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+
+        Date nowDate = new Date();
+        Date expireDate =  dateUtils.getAfterTime(nowDate,30);;// 2小过期
+
+        String token = JWT.create()
+                .withHeader(map)// 设置头部信息 Header
+                .withIssuer("SERVICE")//设置 载荷 签名是有谁生成 例如 服务器
+                .withSubject("this is test token")//设置 载荷 签名的主题
+                // .withNotBefore(new Date())//设置 载荷 定义在什么时间之前，该jwt都是不可用的.
+                .withAudience("APP")//设置 载荷 签名的观众 也可以理解谁接受签名的
+                .withIssuedAt(nowDate) //设置 载荷 生成签名的时间
+                .withExpiresAt(expireDate)//设置 载荷 签名过期的时间
+                .sign(algorithm);//签名 Signature
+        Assert.assertTrue(token.length() > 0);
+        System.out.println(token);
+    }
+}
